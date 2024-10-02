da5bcfab1a0a3aa38202b3ac710302dfc45d4d15
Ryan Townsend <122849562+ryan-townsend-1@users.noreply.github.com>
2024-10-02 13:21:35 -0500

    added bookings and resources to event structure

--- src/types/Requests.ts
+++ src/types/Requests.ts
@@ -1,7 +1,32 @@
+export interface TimeDetails {
+  startDateTime: string;
+  endDateTime: string;
+  allDay?: boolean;
+}
+
+export interface SpaceDetails {
+  spaceId: string;
+  configurationId?: string;
+}
+
+export interface ResourceDetails {
+  resourceId: string;
+  quantity: number;
+}
+
+export interface BookingDetails {
+  bookingTitle?: string;
+  description?: string;
+  timeSpan: TimeDetails;
+  spaces: SpaceDetails[];
+  resources: ResourceDetails[];
+}
+
 export interface EventRequest {
   eventTitle: string;
   description?: string;
-  startDate: string;
-  endDate: string;
-  categoryId?: string;  
+  categoryId?: string;
+  timeSpan: TimeDetails;
+  bookingDetails: BookingDetails[];  
+  additionalInformation?: string;
 }
